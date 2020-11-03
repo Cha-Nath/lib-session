5d7ea33128b9685304d2e31159c67d480707604b
Nath_ <firminchanteux@gmail.com>
2020-11-03 12:04:46 +0100

    add flash feature

--- Traits/SessionTrait.php
+++ Traits/SessionTrait.php
@@ -4,9 +4,23 @@ namespace nlib\Session\Traits;
 
 trait SessionTrait {
 
-    public function sessionStart(string $i = 'i') : void {
+    public function sessionStart() : self {
         if(empty(session_id())) SESSION_START();
-        if(empty($_SESSION[$i])) $_SESSION[$i] = [];
+        if(empty($this->getCurrentSession())) $_SESSION[$this->_i()] = [];
+        return $this;
     }
 
+    #region Getter
+
+    public function getFlash($key) { return array_key_exists($key, $cs = $this->getCurrentSession()) ? $cs[$key] : null ; }
+    
+    public function getCurrentSession() { return array_key_exists($this->_i(), $_SESSION) ? $_SESSION[$this->_i()] : []; }
+
+    #endregion
+
+    #region Setter
+    
+    public function addflash($key, $value) : self { $_SESSION[$this->_i()][$key] = $value; return $this; }
+
+    #endregion
 }
